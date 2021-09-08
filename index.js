0763cacb7e085ef206d8a424d6bce37c7482ba8d
TrentinB <trentin.barnhart@gmail.com>
2021-09-07 23:00:49 -0500

    First pass
    First try at the assignment. May need fixed when it doesn't work right. Who knows. I'm just glad to have something to turn in right now.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const validatePassword = require('./passwordvalidation'); //import the function
 
-// Your code goes here!
+function validatePassword(){
+    console.log("Enter a password to validate (or :q to quit)")
+    var text = input()
+
+
+    if (validatePassword(text) == false && text != ':q') {
+        console.log("The password is invalid: " + text);    // if valid
+    }
+    else if (validatePassword(text) == true && text != ':q') {
+        console.log("The password is valid: " + text);  // if invalid
+    }
+    if (text == ':q'){
+        console.log('Goodbye!');    //if quit
+        return;
+    }
+
+    validatePassword();
+}
---
+++ passwordvalidation.js
@@ -0,0 +1,9 @@
+//Converts a given string (single word), test if valid password
+function validatePassword(word) {
+    return /[a-z]/.test(word) && // A lowercase letter
+        /[A-Z]/.test(word) && // An upppercase letter
+        /[0-9]/.test(word) && // A number
+        word.length >= 8 && // At least 8 characters
+        /\s/.test(word) == false; // An no spaces!
+}
+export {};
