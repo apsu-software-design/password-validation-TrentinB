7ce51bde733d93ac412ee9aeb9e7822c23d97634
TrentinB <trentin.barnhart@gmail.com>
2021-09-09 18:01:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,27 @@ const rl = readline.createInterface({
 });
 
 const main = function(){
-    rl.question("Enter a password to validate (or :q to quit)", (text) => {
+    rl.question("Enter a password to validate (or :q to quit):\n", (text) => {
+
+        //checks input
         if (validatePassword(text) == false && text != ':q') {
-            console.log("The password is invalid: " + text);    // if valid
+            console.log("The password is invalid: " + text);    //if valid
         }
         else if (validatePassword(text) == true && text != ':q') {
-            console.log("The password is valid: " + text);  // if invalid
+            console.log("The password is valid: " + text);  //if invalid
         }
         if (text == ':q'){
             console.log('Goodbye!');    //if quit
-            return;
+
+            //I orginally used a return statement here but after exiting main()
+            //the command line would not exit. I found this function online and I am
+            //not sure if it's good practice or not but it worked for now at least. 
+            process.exit();
         }
 
+        //recursion call
         main();
-    
     });
-
-
-    
 }
 
 main();
